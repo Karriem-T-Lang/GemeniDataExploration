f19cdd5117871bbebf3ff7455fca279bda0be3c0
karriem-lang <328486186+Karriem-T-Lang@users.noreply.github.com>
2026-09-13 18:37:30 -0400

    Initial Commit

---
+++ GemeniDataExploration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GemeniDataExploration
+//
+//  Created by Karriem Lang on 9/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GemeniDataExploration/GemeniDataExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GemeniDataExplorationApp.swift
+//  GemeniDataExploration
+//
+//  Created by Karriem Lang on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct GemeniDataExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
